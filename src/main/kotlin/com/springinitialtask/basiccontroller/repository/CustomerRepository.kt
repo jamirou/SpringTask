bb615d60291cbb715a2c8c8edad0e09298266f6a
jamirou <devjamiro@gmail.com>
2024-02-18 11:11:51 -0300

    Repository custommer add, Customer DC

--- src/main/kotlin/com/springinitialtask/basiccontroller/repository/CustomerRepository.kt
+++ src/main/kotlin/com/springinitialtask/basiccontroller/repository/CustomerRepository.kt
@@ -3,4 +3,4 @@ package com.springinitialtask.basiccontroller.repository
 import com.springinitialtask.basiccontroller.entity.Customer
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface CustomerRepository: JpaRepository<Customer, Long> 
+interface CustomerRepository: JpaRepository<Customer, Long>
